59009ef9c5f54c0ded46747876ba3efc0f0b510b
arsall05 <arsalan.mehmood05@gmail.com>
2021-06-08 07:12:28 +0500

    added fee model

---
+++ models/Facts/fct_fee.sql
@@ -0,0 +1,65 @@
+{{ 
+  config(
+    materialized='incremental',
+    sort=['ProfessionalID','ServiceID','EventID'],
+    partition_by={
+      "field": "AuditCreatedDatetime",
+      "data_type": "datetime",
+      "granularity": "day"
+    }
+  ) 
+}}
+
+
+
+SELECT
+  ProfessionalID,
+  
+  (CASE 
+    WHEN ParsedMetadata[OFFSET(0)]='' 
+      THEN 'Not provided' 
+      ELSE  ParsedMetadata[OFFSET(0)]
+    END
+  ) AS ServiceID,
+  
+  EventID,
+  EventType,
+  CreatedAt,
+  
+  (CASE 
+    WHEN ParsedMetadata[OFFSET(0)]!='' 
+      THEN CAST(ParsedMetadata[OFFSET(3)] AS FLOAT64) 
+      ELSE 0.0 
+   END
+  ) as LeadFee,
+
+  AuditCreatedDatetime
+ 
+FROM
+(
+
+  SELECT
+
+      EventID,
+      EventType,
+      ProfessionalID,
+      CreatedAt,
+      SPLIT(Metadata,'_') as ParsedMetadata,
+      CURRENT_DATETIME() as AuditCreatedDatetime, -- Audit column
+    
+    FROM 
+  
+      {{ref('event_logs_stg')}}
+     
+
+      -- this will only be applied on an incremental run & will filter data early
+      -- {{this}} will give last run date which can then be used to pick CDC records daily
+      {% if is_incremental() %}
+        where PARSE_DATETIME('%Y-%m-%d %H:%M:%S', AuditCreatedDatetime) > 
+          (select max(AuditCreatedDatetime) from {{ this }})
+      {% endif %}
+)
+
+
+
+
